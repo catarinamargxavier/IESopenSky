9ecd9d48cc043c5783aa3967a3fe294387a53110
catarina <>
2018-10-22 02:24:31 +0100

    Ligação à API externa (openSky)

---
+++ OpenSkyRestAPI/src/main/java/com/mycompany/openskyrestapi/Application.java
@@ -0,0 +1,39 @@
+package com.mycompany.openskyrestapi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+    
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+   
+    public static void main (String[] args) {
+        SpringApplication.run(Application.class);        
+    }
+    
+    @Bean
+    public RestTemplate restTemplate (RestTemplateBuilder builder) {
+        return builder.build();
+    }    
+    
+    @Bean
+    public CommandLineRunner run (RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Flight[] quote = restTemplate.getForObject("https://opensky-network.org/api/flights/all?begin=1540036800&end=1540040400", Flight[].class);
+            for (Flight voo: quote) {
+                log.info(voo.toString());
+            }
+        };
+    }
+    
+    
+}
